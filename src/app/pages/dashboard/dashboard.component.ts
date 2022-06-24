ecca40c9ea373fd6eef5af2fcb86002c1c3e00be
Juanda <heyjuns@icloud.com>
2022-06-24 15:58:45 +0700

    dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-interface IBasicRoute {
-  url: string;
-  label: string;
-  iconPath: string;
-}
-const BASE_ICON_PATH = './assets'
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -12,18 +6,6 @@ const BASE_ICON_PATH = './assets'
 })
 export class DashboardComponent implements OnInit {
 
-  pages: IBasicRoute[] = [
-    {
-      url: 'market-insight',
-      label: 'Market Insight',
-      iconPath: `${BASE_ICON_PATH}/dashboard-icon.svg`,
-    },
-    {
-      url: 'report',
-      label: 'Report',
-      iconPath:`${BASE_ICON_PATH}/report.svg`
-    }
-  ]
   constructor() { }
 
   ngOnInit(): void {
